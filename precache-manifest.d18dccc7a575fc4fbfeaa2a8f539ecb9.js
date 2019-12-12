747b8c42bab229cf3becf2ffa93c3a88da2f53aa
Revanth Kumar <revanth0212@gmail.com>
2019-12-11 21:45:44 -0600

    Updates

---
+++ precache-manifest.d18dccc7a575fc4fbfeaa2a8f539ecb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119c5a9a22ef0cf6cb38935bab7eaa3c",
+    "url": "/magento-ar-shopping/index.html"
+  },
+  {
+    "revision": "acb0939c76e9d29f4347",
+    "url": "/magento-ar-shopping/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b44a8d14eb1fc4acf621",
+    "url": "/magento-ar-shopping/static/js/2.8d6d272e.chunk.js"
+  },
+  {
+    "revision": "00964326f809160a4ea9ff345d1fc5c9",
+    "url": "/magento-ar-shopping/static/js/2.8d6d272e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "acb0939c76e9d29f4347",
+    "url": "/magento-ar-shopping/static/js/main.5f3f2ad3.chunk.js"
+  },
+  {
+    "revision": "875bdaa6f2a9651fe06f",
+    "url": "/magento-ar-shopping/static/js/runtime-main.c7c6cd5d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9a76070be5344f1218120d5c20fc9e5.js"
+  "/magento-ar-shopping/precache-manifest.d18dccc7a575fc4fbfeaa2a8f539ecb9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/magento-ar-shopping/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
